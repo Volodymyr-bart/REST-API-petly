46dd062a92d86b6a6e15ebe03564ecec827cd4eb
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-02-07 12:22:29 +0200

    fix

--- controllers/auth/checkEmail.js
+++ controllers/auth/checkEmail.js
@@ -1,17 +1,14 @@
-const { HttpError } = require('../../helpers');
-const { User } = require('../../models/user');
+const { HttpError } = require("../../helpers");
+const { User } = require("../../models/user");
 
 const checkEmail = async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
   if (user) {
-    throw HttpError(
-      409,
-      'Користувач з такою електронною адресою вже зареєстрований у системі'
-    );
+    throw HttpError(409, "Користувач з такою електронною адресою вже зареєстрований у системі");
   }
 
-  res.status(200).json(`OK, ${email} not used yet`);
+  res.status(200).json(`OK, ${email} не використовується`);
 };
 
 module.exports = checkEmail;
