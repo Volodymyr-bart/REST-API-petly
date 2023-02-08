04148a0d033324d57cdde64f6ef779a0165d2c15
Dmitry Riabokon <ryabokon.dn@gmail.com>
2023-02-08 10:22:16 +0200

    fixed get category

--- controllers/notices/getNoticesByCategory.js
+++ controllers/notices/getNoticesByCategory.js
@@ -1,17 +1,11 @@
-const { HttpError } = require('../../helpers');
 const { Notice } = require('../../models');
 
 const getNoticesByCategory = async (req, res, next) => {
-  const { _id } = req.params;
+  const { showCategory } = req.params;
 
   const result = await Notice.find({
-    author: _id,
+    category: showCategory,
   });
-
-  if (!result) {
-    const error = HttpError(404);
-    throw error;
-  }
 
   res.json({ status: 'success', code: 200, result });
 };
