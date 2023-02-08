af52a396bea6f8b2012f7aace273cb1212359801
Dmitry Riabokon <ryabokon.dn@gmail.com>
2023-02-08 13:36:50 +0200

    spart1

--- controllers/notices/addNotice.js
+++ controllers/notices/addNotice.js
@@ -1,7 +1,13 @@
 const { Notice } = require('../../models');
+const { cloudinary } = require('../../helpers');
 
 const addNotice = async (req, res, next) => {
   const { _id } = req.user;
+  const { image } = req.body;
+
+  const avatar = await cloudinary.uploader.upload(image, {
+    folder: 'petAvatars',
+  });
   const result = await Notice.create({ ...req.body, author: _id });
 
   res.status(201).json({
