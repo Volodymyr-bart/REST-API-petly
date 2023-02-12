a2d017f72b7559f144b96f5c81a2582001b52be4
varmtech13 <varmtech.co@gmail.com>
2023-02-12 19:47:30 +0100

    Bug fix for email

--- models/user.js
+++ models/user.js
@@ -7,13 +7,14 @@ const phoneJoi = Joi.extend(require("joi-phone-number"));
 const { handleMongooseError } = require("../helpers");
 
 const emailRegexp =
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  /^([A-Za-z0-9_\-\.]{3,})+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
 const nameRegexp = /^[a-z ,.'-]+$/i;
 
 const passRegexp = /^\S+$/;
 
-const dateRegexp = /(0[1-9]|[12][0-9]|3[01])[- -.](0[1-9]|1[012])[- -.](19|20)\d\d/;
+const dateRegexp =
+  /(0[1-9]|[12][0-9]|3[01])[- -.](0[1-9]|1[012])[- -.](19|20)\d\d/;
 
 const userSchema = new Schema(
   {
@@ -26,6 +27,8 @@ const userSchema = new Schema(
       type: String,
       unique: true,
       match: emailRegexp,
+      minlength: 10,
+      maxlength: 63,
       required: true,
     },
     password: {
@@ -62,7 +65,11 @@ const registerSchema = Joi.object({
     .pattern(nameRegexp)
     .required()
     .messages({ "string.pattern.base": "Лише літери англійського алфавіту" }),
-  email: Joi.string().pattern(emailRegexp).required(),
+  email: Joi.string().pattern(emailRegexp).min(10).max(63).required().messages({
+    "string.pattern.base":
+      "Email не має містити спецсимволів, має починатися від 3 символів перед собачкою",
+    "string.min.base": "Мінімальна довжина 10 символів",
+  }),
   password: Joi.string()
     .pattern(passRegexp)
     .min(7)
@@ -70,7 +77,9 @@ const registerSchema = Joi.object({
     .required()
     .messages({ "string.pattern.base": "Пароль повинен бути без пробілів" }),
   address: Joi.string(),
-  phone: phoneJoi.string().phoneNumber({ defaultCountry: "UA", format: "national", strict: true }),
+  phone: phoneJoi
+    .string()
+    .phoneNumber({ defaultCountry: "UA", format: "national", strict: true }),
 });
 
 const updateSchema = Joi.object({
@@ -79,7 +88,9 @@ const updateSchema = Joi.object({
     .messages({ "string.pattern.base": "Лише літери англійського алфавіту" }),
   email: Joi.string().pattern(emailRegexp),
   address: Joi.string(),
-  phone: phoneJoi.string().phoneNumber({ defaultCountry: "UA", format: "national", strict: true }),
+  phone: phoneJoi
+    .string()
+    .phoneNumber({ defaultCountry: "UA", format: "national", strict: true }),
   birthday: Joi.string().pattern(dateRegexp),
   userAvatar: Joi.string(),
 });
